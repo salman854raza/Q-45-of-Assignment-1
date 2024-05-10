8250258206f60034d6d320d2dd574a7693b6e50a
Salman Raza <raza3213710@gmail.com>
2024-05-10 04:28:39 -0700

    q-45-of-Assingment-1
    1 task and second task changes

---
+++ task 1 hello world/main.js
@@ -0,0 +1,4 @@
+"use strict";
+// This is my first Assignment Task No 2
+// This is Simple Task "Hello World"
+console.log("Hello World");
---
+++ task 1 hello world/main.ts
@@ -0,0 +1,3 @@
+// This is my first Assignment Task No 2
+// This is Simple Task "Hello World"
+console.log("Hello World");
